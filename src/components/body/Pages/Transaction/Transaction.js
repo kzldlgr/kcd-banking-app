e77486b0a38f19e53ae18834b92bc1f834c9f01a
Cee-Jay Malacas <ceejaymalacas17@gmail.com>
2022-11-08 02:15:02 +0800

    minimal changes

--- src/components/body/Pages/Transaction/Transaction.js
+++ src/components/body/Pages/Transaction/Transaction.js
@@ -4,53 +4,54 @@ import './Transaction.css';
 
 export default function Transaction({ children }) {
 
-    const {users, setUsers, userInfo, setUserInfo} = useContext(UsersContext);
+    const { users, setUsers, userInfo, setUserInfo } = useContext(UsersContext);
     const [transaction, setTransaction] = useState([]);
     const currentUser = JSON.parse(sessionStorage.getItem('user'));
-    
+
     useEffect(() => {
 
         if (users === undefined) return
-    
+
         Array.from(users).forEach(client => {
-            if (client.myemail === currentUser.myemail && currentUser.usertype !== 'admin') {
-                
+            if (client.myemail === currentUser.myemail) {
                 setTransaction(client.myhistory.map((e, index) => (
                     <tr key={index}>
                         <td>{e.date}</td>
                         <td>{e.description}</td>
-                        <td>{Number(e.amount).toLocaleString('tl-PH', {style: 'currency', currency: 'PHP',})}</td>
+                        <td>{Number(e.amount).toLocaleString('tl-PH', { style: 'currency', currency: 'PHP', })}</td>
                     </tr>
                 )))
-            }else {
-                setTransaction(userInfo.myhistory.map((e, index) => (
-                    <tr key={index}>
-                    <td>{e.date}</td>
-                    <td>{e.description}</td>
-                    <td>{Number(e.amount).toLocaleString('tl-PH', {style: 'currency', currency: 'PHP',})}</td>
-                </tr>
-                )))
-            }
-        });    
-
-    },[users])
-
-        return (
-            <div className='transaction'>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>DATE</th>
-                            <th>DESCRIPTION</th>
-                            <th>AMOUNT</th>
+            } else {
+                {
+                    setTransaction(userInfo.myhistory.map((e, index) => (
+                        <tr key={index}>
+                            <td>{e.date}</td>
+                            <td>{e.description}</td>
+                            <td>{Number(e.amount).toLocaleString('tl-PH', { style: 'currency', currency: 'PHP', })}</td>
                         </tr>
-                    </thead>
-    
-                    <tbody>
-                        {transaction}
-                    </tbody>
-                </table>
-            </div>
-        )
+                    )))
+                }
+            }
+        });
+
+    }, [users])
+
+    return (
+        <div className='transaction'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>DATE</th>
+                        <th>DESCRIPTION</th>
+                        <th>AMOUNT</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {transaction}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
