573a19a18e36a604c0ff2b9963cc48db16ba5198
Cee-Jay Malacas <ceejaymalacas17@gmail.com>
2022-11-11 22:31:19 +0800

    commit

--- src/components/body/Tablist/Tablist.js
+++ src/components/body/Tablist/Tablist.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import Tabs from './tabs/Tabs';
+import React, { useContext } from "react";
+import Tabs from "./tabs/Tabs";
+import { UsersContext } from "../../../context/UsersContext";
 
+export default function Tablist({ userlevel }) {
+	const { userInfo } = useContext(UsersContext);
 
-
-export default function Tablist({userlevel}) {
-  
-  if (userlevel !== 'admin'){
-    return (
-      <div className='tablist'>
-        <Tabs text='Transaction' path='./Transaction'/>
-        <Tabs text='Deposit' path='./Deposit'/>
-        <Tabs text='Withdraw' path='./Withdraw'/>
-        <Tabs text='Transfer' path='./Transfer'/>
-        <Tabs text='Expenses' path='./Expense'/>
-      </div>
-    )
-  } else {
-    return (
-
-      <div className='tablist'>
-        <Tabs text='Manage User' path='./ManageUser'/>
-        <Tabs text='Transaction' path='./Transaction'/>
-        <Tabs text='Deposit' path='./Deposit'/>
-        <Tabs text='Withdraw' path='./Withdraw'/>
-        <Tabs text='Transfer'path='./Transfer'/>
-        <Tabs text='Add Client' path='./AddClient'/>
-        <Tabs text='Request' path='./UserRequest' />
-      </div>
-    )
-  }
+	if (userlevel !== "admin") {
+		return (
+			<div className="tabs font-pop">
+				<Tabs text="Transaction" path="./Transaction" />
+				<Tabs text="Deposit" path="./Deposit" />
+				<Tabs text="Withdraw" path="./Withdraw" />
+				<Tabs text="Transfer" path="./Transfer" />
+				<Tabs text="Expenses" path="./Expense" />
+				<Tabs text="Paybill" path="./Paybill" />
+			</div>
+		);
+	} else {
+		return (
+			<div className="tabs font-pop w-[45rem] ">
+				{userInfo.firstname && <Tabs text="Edit User" path="./ManageUser" />}
+				{userInfo.firstname && <Tabs text="Transaction" path="./Transaction" />}
+				<Tabs text="Deposit" path="./Deposit" />
+				<Tabs text="Withdraw" path="./Withdraw" />
+				<Tabs text="Transfer" path="./Transfer" />
+				<Tabs text="Add Client" path="./AddClient" />
+				<Tabs text="Request" path="./UserRequest" />
+			</div>
+		);
+	}
 }
