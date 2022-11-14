e6ee4e11d900a6f11d1c7bfcebada443bc84bd86
kzldlgr <kazel_deligero@yahoo.com>
2022-11-14 18:48:17 +0800

    initial commit

--- src/components/Body/Pages/PayBills/Paybills.js
+++ src/components/Body/Pages/PayBills/Paybills.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, useContext } from "react";
 import { UsersContext } from "../../../../context/UsersContext";
-import "./PayBills.css";
+import { useForm } from "react-hook-form";
 import swal from "sweetalert";
 
 export default function Paybills({children}){
     const newDate = new Date()
+    const { register, handleSubmit } = useForm()
     const {users, setUsers, userBalance, setUserBalance, userInfo, setUserInfo} = useContext(UsersContext)
     const [amount, setAmount] = useState([])
-    const [biller, setBiller] = useState([0])
     const [errorMsgs, setErrorMsgs] = useState('')
     const [balanceOutput, setBalanceOutput] = useState([])
     const currentUser = JSON.parse(sessionStorage.getItem('user'))
@@ -26,22 +26,21 @@ export default function Paybills({children}){
         }
     }, [amount])
 
-const adminSide = (client) => {
+const adminSide = (client, data) => {
     if (userInfo !== undefined && userInfo.length !== 0) {
         if (client.myemail === userInfo.myemail) { 
-            if (client.balance >= amount) {
+            if (Number(client.balance) >= amount) {
                 client.myhistory.push({
                     date: `${newDate.getMonth()+1}-${newDate.getDate()}-${newDate.getFullYear()}`,
-                    description: biller + ' Bill Payment from Bankerost main',
+                    description: data.utilities + ' Bill Payment from Bankerost main',
                     amount: amount,
-                    category: '',
-                    type: 'Bill Payment'
+                    category: 'UTI',
+                    type: 'expense'
                 })
                 client.balance -= Number(amount)
+                setUserBalance(balanceOutput);
+                setUsers(users);
                 localStorage.setItem('users', JSON.stringify(users))
-                setUsers(users)
-                setUserBalance(balanceOutput)
-                setAmount('')
                 swal({
                     text: "Successfully paid a bill",
                     icon: "success",
@@ -53,21 +52,21 @@ const adminSide = (client) => {
     }
 }
 
-const clientSide = (client) => {  
+const clientSide = (client, data) => {  
     if (client.myemail === currentUser.myemail) { 
-        if (client.balance >= amount) {
+        if (Number(client.balance) >= amount) {
             client.myhistory.push({
                 date: `${newDate.getMonth()+1}-${newDate.getDate()}-${newDate.getFullYear()}`,
-                description: biller + ' Bill Payment',
+                description: data.utilities + ' Bill Payment',
                 amount: amount,
-                category: '',
-                type: 'Bill Payment'
+                category: 'UTI',
+                type: 'expense'
             })
             client.balance -= Number(amount)
+            setUserBalance(balanceOutput);
+            setUsers(users);
             localStorage.setItem('users', JSON.stringify(users))
-            setUsers(users)
-            setUserBalance(balanceOutput)
-            setAmount('')
+            setUsers(JSON.parse(localStorage.getItem("users")));
             swal({
                 text: "Successfully paid a bill",
                 icon: "success",
@@ -78,47 +77,42 @@ const clientSide = (client) => {
     }
 }
 
-    const onHandleClick = (e) => {
-        e.preventDefault()
+    const onSubmit = (data) => {
         if (amount === '' || amount.length === 0) return
-
         users.forEach(client => {
             if (currentUser.usertype === 'admin'){
-                adminSide(client)
+                adminSide(client, data)
             } else if (currentUser.usertype === 'user'){
-                clientSide(client)
+                clientSide(client, data)
             }
         });
+        setAmount('');
     }
 
     return (
-        <> 
-            <div className='pages'>
-                <div className="paymentContainer">
-                    <h1>Pay Bills</h1>
-                    <br/><br/>
+        
+        <div className='p-5 w-full place-content-center font-pop bg-base-100 rounded-md'>
+            <form className="w-full flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+                <h1 className="font-bold text-lg">Pay Bills</h1>
 
-                    <select value={biller} onChange={(e) => setBiller(e.target.value)}>
-                      <option value="Meralco">Electricity - Meralco</option>
-                      <option value="Luelco">Electricity - Luelco</option>
-                      <option value="Maynilad">Water - Maynilad</option>
-                      <option value="Manila Water">Water - Manila Water</option>
-                      <option value="Globe">Telcos - Globe</option>
-                      <option value="Smart">Telcos - Smart</option>
-                    </select>
+                <select {...register('utilities')} className="select select-bordered w-full">
+                    <option value="Meralco">Electricity - Meralco</option>
+                    <option value="Luelco">Electricity - Luelco</option>
+                    <option value="Maynilad">Water - Maynilad</option>
+                    <option value="Manila Water">Water - Manila Water</option>
+                    <option value="Globe">Telcos - Globe</option>
+                    <option value="Smart">Telcos - Smart</option>
+                </select>
 
-                    <p>Amount</p>
-                    <input type='number' maxLength={10} value={amount} onChange={e => setAmount(e.target.value)}></input>
-                    <p className='errorMsgs'>{errorMsgs}</p>
-                    <p>Balance: {balanceOutput.toLocaleString('tl-PH', {style: 'currency', currency: 'PHP',})}</p>
-                    
-                    <button onClick={onHandleClick} className='confirmbutton'>Confirm</button> 
-                </div>
-                {children}
-            </div>
-        </>
+                <span className="font-bold text-lg">Amount</span>
+                <input className="input input-bordered" type='number' maxLength={10} value={amount} onChange={e => setAmount(e.target.value)}/>
+                <p className='errorMsgs'>{errorMsgs}</p>
+                <p>Balance: {balanceOutput.toLocaleString('tl-PH', {style: 'currency', currency: 'PHP',})}</p>
+                
+                <button type="submit" className='btn btn-primary self-end mt-3'>Confirm</button> 
+            </form>
+            {children}
+        </div>
+        
     )
 }
-
-// --OTP
-// --Validation
