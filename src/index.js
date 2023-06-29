efa7818f0c7674ae16c35450f1f68ff05c4445a1
Kazel Deligero <99521214+kzldlgr@users.noreply.github.com>
2023-06-29 18:17:42 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/indec.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<HashRouter>
+	<BrowserRouter>
 		<App />
-	</HashRouter>
+	</BrowserRouter>
 );
